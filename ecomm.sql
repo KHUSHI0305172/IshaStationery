f02e57520b09104ef998915bd285f2b8335d71f1
KHUSHI0305172 <parikhkhushi52@gmail.com>
2023-08-16 21:20:37 +0530

    first commit

---
+++ ecomm.sql
@@ -0,0 +1,647 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 14, 2022 at 06:23 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ecomm`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin_users`
+--
+
+CREATE TABLE `admin_users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` int(11) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mobile` varchar(50) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `admin_users`
+--
+
+INSERT INTO `admin_users` (`id`, `username`, `password`, `role`, `email`, `mobile`, `status`) VALUES
+(1, 'admin', 'admin', 0, '', '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `banner`
+--
+
+CREATE TABLE `banner` (
+  `id` int(11) NOT NULL,
+  `heading1` varchar(255) NOT NULL,
+  `heading2` varchar(255) NOT NULL,
+  `btn_txt` varchar(255) DEFAULT NULL,
+  `btn_link` varchar(55) DEFAULT NULL,
+  `image` varchar(255) NOT NULL,
+  `order_no` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `banner`
+--
+
+INSERT INTO `banner` (`id`, `heading1`, `heading2`, `btn_txt`, `btn_link`, `image`, `order_no`, `status`) VALUES
+(3, 'New product are added every week', 'Widest Range', 'View More', 'http://localhost/IshaStationery/categories.php?id=5', '344144717_3236083_stationery2-1.jpg', 0, 1),
+(4, 'Get Best Rates', 'Office Stationery', 'View More', 'http://localhost/IshaStationery/categories.php?id=5&sub', '709339685_921020_stationery2-4.jpg', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `categories` varchar(255) NOT NULL,
+  `status` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `categories`
+--
+
+INSERT INTO `categories` (`id`, `categories`, `status`) VALUES
+(5, 'Categories', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `color_master`
+--
+
+CREATE TABLE `color_master` (
+  `id` int(11) NOT NULL,
+  `color` varchar(255) NOT NULL,
+  `status` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contact_us`
+--
+
+CREATE TABLE `contact_us` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(75) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `comment` text NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `coupon_master`
+--
+
+CREATE TABLE `coupon_master` (
+  `id` int(11) NOT NULL,
+  `coupon_code` varchar(50) NOT NULL,
+  `coupon_value` int(11) NOT NULL,
+  `coupon_type` varchar(10) NOT NULL,
+  `cart_min_value` int(11) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order`
+--
+
+CREATE TABLE `order` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `address` varchar(250) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `pincode` int(11) NOT NULL,
+  `payment_type` varchar(20) NOT NULL,
+  `total_price` float NOT NULL,
+  `payment_status` varchar(20) NOT NULL,
+  `order_status` int(11) NOT NULL,
+  `length` float NOT NULL,
+  `breadth` float NOT NULL,
+  `height` float NOT NULL,
+  `weight` float NOT NULL,
+  `txnid` varchar(200) NOT NULL,
+  `mihpayid` varchar(200) NOT NULL,
+  `ship_order_id` int(11) NOT NULL,
+  `ship_shipment_id` int(11) NOT NULL,
+  `payu_status` varchar(10) NOT NULL,
+  `coupon_id` int(11) NOT NULL,
+  `coupon_value` varchar(50) NOT NULL,
+  `coupon_code` varchar(50) NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order`
+--
+
+INSERT INTO `order` (`id`, `user_id`, `address`, `city`, `pincode`, `payment_type`, `total_price`, `payment_status`, `order_status`, `length`, `breadth`, `height`, `weight`, `txnid`, `mihpayid`, `ship_order_id`, `ship_shipment_id`, `payu_status`, `coupon_id`, `coupon_value`, `coupon_code`, `added_on`) VALUES
+(1, 0, '', '', 0, 'payu', 0, 'Success', 5, 0, 0, 0, 0, '', '', 0, 0, '', 0, '', '', '0000-00-00 00:00:00'),
+(2, 4, 'abc', 'acb', 123, 'COD', 139, 'pending', 1, 0, 0, 0, 0, '44a4a4e84d9907d10707', '', 0, 0, '', 0, '', '', '2022-03-14 06:06:44'),
+(3, 4, 'abc', 'acb', 123, 'COD', 139, 'pending', 1, 0, 0, 0, 0, 'e6689934cad49da196c9', '', 0, 0, '', 0, '', '', '2022-03-14 06:09:28'),
+(4, 4, 'abc', 'acb', 123, 'COD', 139, 'pending', 1, 0, 0, 0, 0, '39714d008f6b32674f34', '', 0, 0, '', 0, '', '', '2022-03-14 06:09:30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_detail`
+--
+
+CREATE TABLE `order_detail` (
+  `id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `product_attr_id` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `order_detail`
+--
+
+INSERT INTO `order_detail` (`id`, `order_id`, `product_id`, `product_attr_id`, `qty`, `price`) VALUES
+(1, 1, 7, 5, 10, 333),
+(2, 2, 20, 13, 1, 139),
+(3, 3, 20, 13, 1, 139),
+(4, 4, 20, 13, 1, 139);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_status`
+--
+
+CREATE TABLE `order_status` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `order_status`
+--
+
+INSERT INTO `order_status` (`id`, `name`) VALUES
+(1, 'Pending'),
+(2, 'Processing'),
+(3, 'Shipped'),
+(4, 'Canceled'),
+(5, 'Complete');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(11) NOT NULL,
+  `categories_id` int(11) NOT NULL,
+  `sub_categories_id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `mrp` float NOT NULL,
+  `price` float NOT NULL,
+  `qty` int(11) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `short_desc` varchar(2000) NOT NULL,
+  `description` text NOT NULL,
+  `best_seller` int(11) NOT NULL,
+  `meta_title` varchar(2000) NOT NULL,
+  `meta_desc` varchar(2000) NOT NULL,
+  `meta_keyword` varchar(2000) NOT NULL,
+  `added_by` int(11) NOT NULL,
+  `status` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `categories_id`, `sub_categories_id`, `name`, `mrp`, `price`, `qty`, `image`, `short_desc`, `description`, `best_seller`, `meta_title`, `meta_desc`, `meta_keyword`, `added_by`, `status`) VALUES
+(18, 5, 4, 'Apsara Wax Crayons - 24 shades (Pack of 5)', 0, 0, 0, '376024411_crayons.jpeg', 'Introduce your child to the world of colours with these Apsara wax caryon colours. Let them express their ideas & thoughts and hand them with this 24 colour palette.', 'Introduce your child to the world of colours with these Apsara wax caryon colours. Let them express their ideas & thoughts and hand them with this 24 colour palette.', 0, 'Apsara Wax Crayons - 24 shades (Pack of 5)', 'Apsara Wax Crayons - 24 shades (Pack of 5)', 'Apsara Wax Crayons - 24 shades (Pack of 5)', 1, 1),
+(19, 5, 4, 'Camel water colours tubes 14 shades', 0, 0, 0, '954746530_A1YaFDzMiPL._SX425_-228x228.jpg', 'Now create impressive textures with these Camel Water Colour Tubes. This pack comes with 14 water colour tubes of 5 ml each. Shades: - Chinese White - Prussian Blue - Cobalt Blue Hue - Chrome Yellow Huw - Lemon Yellow - Yellow Ochre - Vermillion Hue - Carmine - Viridian Hue - Chrome Green Light Hue - Burnt Sienna - Ivory Black - Flourescent Yellow - Flourescent Orange', 'Now create impressive textures with these Camel Water Colour Tubes. This pack comes with 14 water colour tubes of 5 ml each. Shades: - Chinese White - Prussian Blue - Cobalt Blue Hue - Chrome Yellow Huw - Lemon Yellow - Yellow Ochre - Vermillion Hue - Carmine - Viridian Hue - Chrome Green Light Hue - Burnt Sienna - Ivory Black - Flourescent Yellow - Flourescent Orange', 1, 'Camel water colours tubes 14 shades', 'Camel water colours tubes 14 shades', 'Camel water colours tubes 14 shades', 1, 1),
+(20, 5, 4, 'Faber Castell Oil Pastels 50 shades', 0, 0, 0, '628816404_faber-castell-oil-pastels-228x228.jpg', 'Ideal for soft blending, scartch effect, sgraffito, tinting and shading, this pack of 50 oil pastel colours offers unmatched colouring experience.', 'Ideal for soft blending, scartch effect, sgraffito, tinting and shading, this pack of 50 oil pastel colours offers unmatched colouring experience.', 0, 'Faber Castell Oil Pastels 50 shades', 'Faber Castell Oil Pastels 50 shades', 'Faber Castell Oil Pastels 50 shades', 1, 1),
+(21, 5, 5, 'Combo - Art File + Sketch Pens + Acrylic Colours + Colour Palette + Painting Brush', 0, 0, 0, '426615785_sh969-228x228.jpg', 'A perfect combo for art lovers having art file, colours and painting brush. So buy this complete drawing set.', '- A3 size sketch book with 30 sheets of 145 GSM\r\n- Stic Sketch Pens Mini (12 colours)\r\n- Fevicryl Acrylic Colours Lilac Kit 6 Shades\r\n- Artist Palette (12 Colours)\r\n- Painting Brush set of 7 round brushes', 1, 'Combo - Art File + Sketch Pens + Acrylic Colours + Colour Palette + Painting Brush', 'Combo - Art File + Sketch Pens + Acrylic Colours + Colour Palette + Painting Brush', 'Combo - Art File + Sketch Pens + Acrylic Colours + Colour Palette + Painting Brush', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_attributes`
+--
+
+CREATE TABLE `product_attributes` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `size_id` int(11) NOT NULL,
+  `color_id` int(11) NOT NULL,
+  `mrp` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product_attributes`
+--
+
+INSERT INTO `product_attributes` (`id`, `product_id`, `size_id`, `color_id`, `mrp`, `price`, `qty`) VALUES
+(11, 18, 0, 0, 100, 98, 1),
+(12, 19, 0, 0, 110, 84, 20),
+(13, 20, 0, 0, 150, 139, 24),
+(14, 21, 0, 0, 572, 368, 12),
+(15, 22, 0, 0, 122, 12, 25);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_images`
+--
+
+CREATE TABLE `product_images` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `product_images` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product_images`
+--
+
+INSERT INTO `product_images` (`id`, `product_id`, `product_images`) VALUES
+(1, 8, '479197953_526258680_Floral-Print-Polo-T-shirt1.jpg'),
+(2, 8, '301120849_309027777_Floral-Print-Polo-T-shirt.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product_review`
+--
+
+CREATE TABLE `product_review` (
+  `id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `rating` varchar(20) NOT NULL,
+  `review` text NOT NULL,
+  `status` int(11) NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `product_review`
+--
+
+INSERT INTO `product_review` (`id`, `product_id`, `user_id`, `rating`, `review`, `status`, `added_on`) VALUES
+(2, 9, 1, 'Good', 'asAS', 0, '2021-05-05 08:31:39');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shiprocket_token`
+--
+
+CREATE TABLE `shiprocket_token` (
+  `id` int(11) NOT NULL,
+  `token` varchar(500) NOT NULL,
+  `added_on` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `shiprocket_token`
+--
+
+INSERT INTO `shiprocket_token` (`id`, `token`, `added_on`) VALUES
+(1, '', '2019-04-09 00:28:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `size_master`
+--
+
+CREATE TABLE `size_master` (
+  `id` int(11) NOT NULL,
+  `size` varchar(255) NOT NULL,
+  `status` tinyint(4) NOT NULL,
+  `order_by` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_categories`
+--
+
+CREATE TABLE `sub_categories` (
+  `id` int(11) NOT NULL,
+  `categories_id` int(11) NOT NULL,
+  `sub_categories` varchar(100) NOT NULL,
+  `status` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sub_categories`
+--
+
+INSERT INTO `sub_categories` (`id`, `categories_id`, `sub_categories`, `status`) VALUES
+(1, 2, 'T-Shirt', 1),
+(2, 2, 'Trouser', 1),
+(3, 4, 'Shirt', 1),
+(4, 5, 'Art & Craft Supplies', 1),
+(5, 5, 'Combo Offers', 1),
+(6, 5, 'Desk Accessories', 1),
+(7, 5, 'Files and Folders', 1),
+(8, 5, 'Notebooks & Diaries', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wishlist`
+--
+
+CREATE TABLE `wishlist` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `product_id` int(11) NOT NULL,
+  `added_on` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `wishlist`
+--
+
+INSERT INTO `wishlist` (`id`, `user_id`, `product_id`, `added_on`) VALUES
+(1, 1, 12, '2021-04-08 01:53:31');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin_users`
+--
+ALTER TABLE `admin_users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `banner`
+--
+ALTER TABLE `banner`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `color_master`
+--
+ALTER TABLE `color_master`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `coupon_master`
+--
+ALTER TABLE `coupon_master`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order`
+--
+ALTER TABLE `order`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `order_status`
+--
+ALTER TABLE `order_status`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_attributes`
+--
+ALTER TABLE `product_attributes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_images`
+--
+ALTER TABLE `product_images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product_review`
+--
+ALTER TABLE `product_review`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `shiprocket_token`
+--
+ALTER TABLE `shiprocket_token`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `size_master`
+--
+ALTER TABLE `size_master`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sub_categories`
+--
+ALTER TABLE `sub_categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin_users`
+--
+ALTER TABLE `admin_users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `banner`
+--
+ALTER TABLE `banner`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `color_master`
+--
+ALTER TABLE `color_master`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `contact_us`
+--
+ALTER TABLE `contact_us`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `coupon_master`
+--
+ALTER TABLE `coupon_master`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `order`
+--
+ALTER TABLE `order`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `order_detail`
+--
+ALTER TABLE `order_detail`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `order_status`
+--
+ALTER TABLE `order_status`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT for table `product_attributes`
+--
+ALTER TABLE `product_attributes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+
+--
+-- AUTO_INCREMENT for table `product_images`
+--
+ALTER TABLE `product_images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `product_review`
+--
+ALTER TABLE `product_review`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `shiprocket_token`
+--
+ALTER TABLE `shiprocket_token`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `size_master`
+--
+ALTER TABLE `size_master`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `sub_categories`
+--
+ALTER TABLE `sub_categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `wishlist`
+--
+ALTER TABLE `wishlist`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
